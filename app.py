96370964036c591dd87682b3ff66073b994903a0
GeryGreyhound <gergely.tomanovics@gmail.com>
2021-12-14 19:27:17 +0100

    bug fix

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ import os
 import psycopg2
 from flask import Flask, Markup, jsonify
 from passlib.hash import sha256_crypt
+import urlparse
 
 url = urlparse.urlparse(os.environ.get('DATABASE_URL'))
 db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
 schema = "schema.sql"
 conn = psycopg2.connect(db)
-
 cur = conn.cursor()
-x = os.environ.get('DATABASE_URL')
 
 app = Flask(__name__)
 
