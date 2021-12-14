f7470de2d016909b461882b7f857bc1883cca6ab
GeryGreyhound <gergely.tomanovics@gmail.com>
2021-12-14 19:22:30 +0100

    testing database setup

--- app.py
+++ app.py
@@ -1,31 +1,23 @@
 import os
 import psycopg2
-from flask import Flask, Markup
+from flask import Flask, Markup, jsonify
+from passlib.hash import sha256_crypt
+
+url = urlparse.urlparse(os.environ.get('DATABASE_URL'))
+db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
+schema = "schema.sql"
+conn = psycopg2.connect(db)
+
+cur = conn.cursor()
+x = os.environ.get('DATABASE_URL')
 
 app = Flask(__name__)
 
-'''
-
-class DatabaseConnection:
-
-	config_file = "database.conf"
-	
-	with open(config_file) as config:
-	    db_config = config.read()
-	
-	connection = psycopg2.connect(db_config)
-	connection.autocommit = True
-	cursor = connection.cursor()
-
-dbc = DatabaseConnection()
-
-dbc.cursor.execute("SELECT * FROM coinbase_pro LIMIT 1")
-test = dbc.cursor.fetchone()
-
-'''
-
-x = os.environ.get('DATABASE_URL')
-
 @app.route("/")
 def index():
+
+	cur.execute("SELECT * FROM secrets")
+
+	x = cur.fetchone()
+
     return "Szia Uram! " + str(x)
