3e511023349c8a452a994a1a6c9f7acbeb0f3785
GeryGreyhound <gergely.tomanovics@gmail.com>
2021-12-15 05:48:56 +0100

    ...

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import os
 import psycopg2
-from flask import Flask, Markup, jsonify
+from flask import Flask, Markup, jsonify, request
 from passlib.hash import sha256_crypt
 from urllib.parse import urlparse
 
-url = urlparse(os.environ.get('DATABASE_URL'))
-db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
-schema = "schema.sql"
-conn = psycopg2.connect(db)
-cur = conn.cursor()
+from secret import *
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-	cur.execute("SELECT * FROM secrets")
-	x = cur.fetchone()
-	return "Szia Uram! " + str(x)
+def test():
+	s = Secret()
+	return s
+
+
+@app.route("/secret", methods = ["POST", "GET"])
+def secret_page():
+	if request.method == "POST":
+		return "post"
+	else:
+		return "get"
+
+@app.route("/secret/<hash>")
+def retrieve_secret(hash):
+	cur.execute("SELECT * FROM secrets WHERE")
